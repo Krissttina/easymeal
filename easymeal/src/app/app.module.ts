7cd83c02a4f836ecd7f3891c7538b7d1f1d647a0
Kristina <krismetodieva123@gmail.com>
2023-07-23 14:42:01 +0300

    structure update

--- easymeal/src/app/app.module.ts
+++ easymeal/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
 import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
