7684b9a1807b6dc43299f251c373724036e57e3c
Kristina <krismetodieva123@gmail.com>
2023-07-24 19:40:15 +0300

    validations started

--- easymeal/src/app/user/login/login.component.ts
+++ easymeal/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  onLogin(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
+    //this.userService.login();
+    this.router.navigate(["/"]);
+  }
 }
--- easymeal/src/app/user/register/register.component.ts
+++ easymeal/src/app/user/register/register.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+//import { DEFAULT_EMAIL_DOMAINS } from "src/app/shared/constants";
+//import { appEmailValidator } from "src/app/shared/validators/app-email-validator";
+//import { matchPasswordsValidator } from "src/app/shared/validators/match-pswds";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ["", [Validators.required, Validators.minLength(5)]],
+    email: [
+      "",
+      [Validators.required],
+    ],
+    passGroup: this.fb.group(
+      {
+        password: ["", [Validators.required, Validators.minLength(5)]],
+        rePassword: ["", [Validators.required]],
+      },
+      {
+       // validators: [matchPasswordsValidator("password", "rePassword")],
+      }
+    ),
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onRegister(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+  }
+}
+function matchPasswordsValidator(arg0: string, arg1: string): any {
+  throw new Error('Function not implemented.');
 }
