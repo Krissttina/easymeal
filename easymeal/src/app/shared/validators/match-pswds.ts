44b17d9d8b230a8c7ba508cd888b97323900d8ae
Kristina <krismetodieva123@gmail.com>
2023-07-29 20:30:27 +0300

    files new set up

--- easymeal/src/app/shared/validators/match-pswds.ts
+++ easymeal/src/app/shared/validators/match-pswds.ts
@@ -1,16 +1,15 @@
 import { FormGroup, ValidatorFn } from "@angular/forms";
 
 export function matchPasswordsValidator(
-  passwordCtrlOne: string,
-  passwordCtrlTwo: string
-): ValidatorFn {
-  return (control) => {
-    const group = control as FormGroup;
-    const passCtrl1 = group.get(passwordCtrlOne);
-    const passCtrl2 = group.get(passwordCtrlTwo);
-
-    return passCtrl1?.value === passCtrl2?.value
-      ? null
-      : { matchPasswordsValidator: true };
-  };
+    passValue: string, rePassValue: string
+    ): ValidatorFn{
+    return (control)=>{
+        const group = control as FormGroup;
+        const pass1 = group.get(passValue);
+        const pass2 = group.get(rePassValue);
+        //
+        return pass1?.value === pass2?.value
+        ? null
+        : { matchPasswordsValidator: true };
+    }
 }
