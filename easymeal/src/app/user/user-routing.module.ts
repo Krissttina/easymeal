2951d387d4d70ab5ba5c0d6b116a2d1771b33d3b
Kristina <krismetodieva123@gmail.com>
2023-08-06 14:07:19 +0300

    save

--- easymeal/src/app/user/user-routing.module.ts
+++ easymeal/src/app/user/user-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
-    // canActivate: [AuthActivate],
+    //canActivate: [AuthActivate],
   },
   {
     path: 'profile',
