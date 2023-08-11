be91fb1ce28fc2d07efc711c4022b99c8116ecd4
Kristina <krismetodieva123@gmail.com>
2023-08-11 13:33:52 +0300

    take data correctly

--- Rest-api/controllers/recipeController.js
+++ Rest-api/controllers/recipeController.js
@@ -1,4 +1,6 @@
 const { recipeModel } = require('../models/recipeModel');
+const { recipeManager } = require('../manager/recipeManager');
+
 //const { newPost } = require('./postController')
 
 function getAllRecipes(req, res, next) {
@@ -21,11 +23,35 @@ function getRecipe(req, res, next) {
         .then(recipe => res.json(recipe))
         .catch(next);
 }
+ function createRecipe(req, res, next) {
+    const {  
+        name,
+        image,
+        ingediants,
+        instructions,
+        prepTime,
+        cookTime,
+        servings 
+    } = req.body;
 
-function createRecipe(req, res, next) {
-    const recipeData = req.body;
+    console.log( {name, image,
+        ingediants, instructions,
+        prepTime, cookTime, servings});
 
-    recipeModel.create({recipeData});
+    recipeManager.create({
+    name,
+    image,
+    ingediants,
+    instructions,
+    prepTime,
+    cookTime,
+    servings
+    })
+    .then(data => { //error
+        console.log(`Saved successfully ${data}`);
+        res.status(200).json(data)
+    }).catch(next);
+       // res.send({error: err, msg: "Something went wrong"})
 }
 
 module.exports = {
